82bdc5646abef242d4f7f846a1c49906edbc09fe
Naman Arora <71423632+namanArora1022@users.noreply.github.com>
2021-07-31 19:25:32 +0530

    update

--- src/data/home.js
+++ src/data/home.js
@@ -1,6 +1,6 @@
 const HomeObj = {
     headline1: `The Byte Knights`,
-    paragraph: `The byte knight club which is known for their wonderful and attractive event which attracts students to participate in different cocerculum activities which provides fun, creation,of new ideas and thoughts.`,
+    paragraph: `The Byte Knight club which is known for their wonderful and attractive event which attracts students to participate in different cocerculum activities which provides fun, creation,of new ideas and thoughts.`,
     btnText: 'Read more',
     headline2: 'Our lastest Event',
     noEventText: 'No Event Right now 😥'
